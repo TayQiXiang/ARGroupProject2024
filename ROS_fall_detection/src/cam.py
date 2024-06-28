7e63302f2730036cf7f600797d9683e1da01af4e
Ubuntu <u2000490@siswa.um.edu.my>
2024-06-29 02:43:13 +0800

    Clean code

--- ROS_fall_detection/src/cam.py
+++ ROS_fall_detection/src/cam.py
@@ -13,11 +13,9 @@ def det_callback(data):
     try:
         global show_image
         show_image = np.frombuffer(data.data, dtype=np.uint8).reshape((data.height, data.width, -1))
-        #print(show_image.shape)
-        # show_image = bridge.imgmsg_to_cv2(data, "bgr8")
+
     except CvBridgeError as e:
         print(e)
-    #cv2.imshow("Result window", show_image)
 
 
 def image_pub():
@@ -29,25 +27,23 @@ def image_pub():
     show_image = np.array([])
 
     cameraCapture = cv2.VideoCapture(-1)
-    #cv2.namedWindow('Camera')
     bridge = CvBridge()
 
     while not rospy.is_shutdown():
         time_str = "time %s" % rospy.get_time()
-        #rospy.loginfo(time_str)
+
         success, frame = cameraCapture.read()
-        #cv2.imshow('Camera', frame)
+
         while success and cv2.waitKey(1) == -1:
             start = time()
-            #cv2.imshow('Camera', frame)
+
             success, frame = cameraCapture.read()
             image_message = bridge.cv2_to_imgmsg(frame, encoding="passthrough")
             pub.publish(image_message)
-            #rate.sleep()
+
             #global show_image
             if show_image.any():
                 cv2.imshow('Camera', show_image)
-                #cv2.imshow('Capture', frame)
             end = time()
             run_time = end-start
             print(1. / run_time)
